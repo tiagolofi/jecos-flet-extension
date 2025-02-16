d14f6b73e8fe4cba0e052acbf6e53a815e4fdd45
tiagolofi <jejeco75@gmail.com>
2025-02-16 16:17:05 -0300

    adicionando arquivos

--- src/main.py
+++ src/main.py
@@ -1,26 +1,11 @@
+
 import flet as ft
 
-
 def main(page: ft.Page):
-    counter = ft.Text("0", size=50, data=0)
 
-    def increment_click(e):
-        counter.data += 1
-        counter.value = str(counter.data)
-        counter.update()
+    page.title = "Demo App"
 
-    page.floating_action_button = ft.FloatingActionButton(
-        icon=ft.Icons.ADD, on_click=increment_click
-    )
-    page.add(
-        ft.SafeArea(
-            ft.Container(
-                counter,
-                alignment=ft.alignment.center,
-            ),
-            expand=True,
-        )
-    )
+    page.add()
 
-
+# Run the app flet run --web --port 8080
 ft.app(main)
